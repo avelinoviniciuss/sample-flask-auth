284e6150733eaf79ed8b722b2088066f0893ee89
Vinícius <vinicius.silva120@gmail.com>
2024-07-24 08:53:25 -0300

    feat: Add database connection
    It adds the database connection to the app.
    - It creates the user table and its fields.
    - Refact database files.

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from database import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    """
+    User model class for storing user related details.
+    It is mapping id, username, password to the user table.
+    """
+    id: int = db.Column(db.Integer, primary_key=True)
+    username: str = db.Column(db.String(80), nullable=False, unique=True)
+    password: str = db.Column(db.String(80), nullable=False)
