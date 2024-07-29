57af67caea1f2968a7628cd1b63ecc7fcd60d12b
Vinícius <vinicius.silva120@gmail.com>
2024-07-29 08:40:23 -0300

    feat: Add logout and create user methods
    It adds the logout and create user methods to application.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from urllib import request
 from flask import Flask, request, jsonify, Response, make_response
 from database import db
 from models.user import User
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
@@ -47,6 +47,36 @@ def login() -> Response:
             return make_response(jsonify({'message': 'User authenticated successfully'}))
     return make_response(jsonify({'message': 'Invalid credentials'}), 400)
 
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout() -> Response:
+    """
+    Logout route for the user to de-authenticate.
+    Returns:
+        Response: Message with the status of the de-authentication
+    """
+    logout_user()
+    return make_response(jsonify({'message': 'User logged out successfully'}))
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    """
+    Create a new user.
+    Returns:
+        Response: Message with the status of the user creation
+    """
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'User created successfully'}))
+    return make_response(jsonify({'message': 'Invalid data'}), 400)
 
 if __name__ == '__main__':
     app.run(debug=True)
